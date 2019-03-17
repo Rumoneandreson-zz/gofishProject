9430a9da3c8d02957b139d5bfbcac847602299ec
rumoneanderson <rumoneandersonbiz@gmail.com>
2019-03-17 17:10:13 -0500

    Initial GUI complete

--- src/playGame/Main.java
+++ src/playGame/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("playGame.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Go Fish");
+        primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
 
